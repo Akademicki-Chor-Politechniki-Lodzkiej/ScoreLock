1e52c5502ab966ae0de20ee9604366dce6fd5460
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-12-13 22:18:02 +0100

    Added class Admin

--- models.py
+++ models.py
@@ -6,3 +6,17 @@ import secrets
 
 db = SQLAlchemy()
 
+class Admin(UserMixin, db.Model):
+    __tablename__ = 'admins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
