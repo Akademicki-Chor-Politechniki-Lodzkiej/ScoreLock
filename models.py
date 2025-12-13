a03f7cb23e81bda3644cbb7302bf30cc67af5a02
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-12-13 22:17:44 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+
+db = SQLAlchemy()
+
